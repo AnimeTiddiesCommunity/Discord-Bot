eb273d36dcb747f7e675cd4021be7e779ce18345
Azamorn <azamorn@gmail.com>
2021-05-28 22:35:53 +0200

    Moved price fetching to seperate api

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
 require('dotenv').config();
 const axios = require('axios').default;
 const path = require('path'), fs = require('fs');
-const requestBNB = fs.readFileSync(path.join(__dirname, 'request-bnb.txt')).toString();
-const requestTiddy = fs.readFileSync(path.join(__dirname, 'request-tiddy.txt')).toString();
 const buyText = fs.readFileSync(path.join(__dirname, 'buy.txt')).toString();
 const Discord = require('discord.js');
 const discord_bot = new Discord.Client();
 const discordChannels = {
     price_watch: { id: "842538295170564157", instance: null}
 };
-var lastPrice = 0,
-latestPrice = 0;
 
 discord_bot.login(process.env.DISCORD_BOT_TOKEN);
+
 discord_bot.on('ready', async () => {
     discordChannels.price_watch.instance = await discord_bot.channels.fetch(discordChannels.price_watch.id);
-    setInterval(setLatestPrice, process.env.AUTO_ECHO_PRICE_INTERVAL * 1000);
+    setInterval(updateBotPriceNick, process.env.AUTO_ECHO_PRICE_INTERVAL * 1000);
 });
-discord_bot.on('message', msg => {
+
+discord_bot.on('message', async (msg) => {
     if(msg.content.substr(0,1) == '$'){
         if(msg.content.substr(0,6).toUpperCase() == '$PRICE'){
-            if(latestPrice == 0){
-                setLatestPrice().then(() => {
-                    msg.channel.send(getLatestPriceMessage());
-                })
-            }
-            else{
-                msg.channel.send(getLatestPriceMessage());
-            }
+            let tiddies_price = await get_tiddies_latest_price();
+            msg.channel.send(getLatestPriceMessage(tiddies_price));
         }
         else if(msg.content.substr(0,4).toUpperCase() == '$BUY'){
             msg.channel.send(buyText);
@@ -35,68 +27,35 @@ discord_bot.on('message', msg => {
     }
 });
 
-function getLatestPriceMessage(){
-    var priceMovement = lastPrice > latestPrice ? '📉' : '📈';
-    var pricePercentage = lastPrice > 0 ? ((latestPrice / lastPrice) * 100).toFixed(1) : 100.0;
-    var priceMovePercentage = lastPrice > latestPrice ? `-${pricePercentage}%` : `+${pricePercentage}%`;
-    pricePercentage -= 100;
-    return `AnimeTiddies Live Price
-    ${priceMovement} $${latestPrice.toFixed(8)} ${pricePercentage >= 0.5 || pricePercentage <= -0.5 ? ` | ${priceMovePercentage}` : ''}`;
+function get_tiddies_latest_price(){
+    return new Promise((resolve) => {
+        Promise.all([
+            axios.get('http://localhost:8001/coins'),
+            axios.get('http://localhost:8001/tiddies-price')
+        ]).then(([coins_response, tiddies_response]) => {
+            let bnb_price, tiddies_per_bnb = tiddies_response.data;
+            for(let i = 0, n = coins_response.data.length; i < n; i++){
+                if(coins_response.data[i].symbol == 'BNB'){
+                    bnb_price = coins_response.data[i].price;
+                    break;
+                }
+            }
+            resolve((bnb_price / tiddies_per_bnb).toFixed(8))
+        });
+    })
 }
 
-function setLatestPrice(){
+function getLatestPriceMessage(current_price){
+    return `AnimeTiddies Live Price
+    $${current_price.toFixed(2)}`;
+}
+
+async function updateBotPriceNick(){
     return new Promise(async (resolve) => {
-        let [bnb_price, tiddies_per_bnb] = await Promise.all([getBNBPrice(), getTiddiesPerBNB()]);
-        let pancakeswap_price = tiddies_per_bnb*0.9975;
-        let newPrice = ((1/pancakeswap_price)*bnb_price);
-        lastPrice = latestPrice;
-        latestPrice = newPrice;
-        let priceMovement = lastPrice > latestPrice ? '📉' : '📈';
-        fs.writeFileSync(path.join(__dirname,'latest-price.json'), JSON.stringify({
-            last_price: lastPrice,
-            latest_price: latestPrice,
-            price_movement: priceMovement
-        }))
+        let tiddies_price = await get_tiddies_latest_price();
         let guild = await discord_bot.guilds.fetch("842534142327259147");
         let member = await guild.members.fetch(discord_bot.user.id);
-        member.setNickname(`${priceMovement} $${latestPrice.toFixed(8)}`);
+        member.setNickname(`$${tiddies_price.toFixed(8)}`);
         resolve();
     })
 }
-
-function getBNBPrice(){
-    return new Promise((resolve) => {
-        axios.request({
-            method: 'POST',
-            responseType: 'json',
-            headers: {
-                'Accept' : 'application/json',
-                'Content-Type' : 'application/json',
-                'X-API-KEY': process.env.BITQUERY_API_TOKEN
-            },
-            url: 'https://graphql.bitquery.io',
-            data: JSON.stringify({ query: requestBNB.replace(/\r\n/g,"\n"), variables: {} })
-        }).then((response) => {
-            resolve(response.data.data.ethereum.dexTrades[0].quotePrice);
-        })
-    })
-}
-
-function getTiddiesPerBNB(){
-    return new Promise((resolve) => {
-        axios.request({
-            method: 'POST',
-            responseType: 'json',
-            headers: {
-                'Accept' : 'application/json',
-                'Content-Type' : 'application/json',
-                'X-API-KEY': process.env.BITQUERY_API_TOKEN
-            },
-            url: 'https://graphql.bitquery.io',
-            data: JSON.stringify({ query: requestTiddy.replace(/\r\n/g,"\n"), variables: {} })
-        }).then((response) => {
-            resolve(response.data.data.ethereum.dexTrades[0].quotePrice);
-        })
-    })
-}
-
